973bc6b2e924e2b0931e192f0329ba96ff899482
syahril231072 <syahril231072@gmail.com>
2021-07-27 17:54:44 +0700

    fourth

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -77,6 +77,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warn('Invalid Login ................')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
